56e4758d2d556fe0b1b316330bad4183735c6fb1
Bilal Bejja <72463941+bilalbejja-collab@users.noreply.github.com>
2021-05-25 16:55:33 +0200

    haciendo la parte de proyectos

---
+++ src/components/portfolio/Project.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export default class Project extends Component {
+  state = {
+    showInfo: false
+  };
+
+  handleInfo = () => {
+    
+  }
+
+  render() {
+    let { name, languagesIcons, source, info, picture } = this.props.item;
+
+    return (
+      <div className="project">
+        <div className="icons">
+          {languagesIcons.map((icon) => (
+            <i className={icon} key={icon}></i>
+          ))}
+        </div>
+        <h3>{name}</h3>
+        <img src={picture} alt="" />
+        <span className="infos">
+          <i className="fas fa-plus-circle"></i>
+        </span>
+      </div>
+    );
+  }
+}
