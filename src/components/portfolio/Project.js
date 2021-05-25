055240e9eaee928fa85295daa1dc54c6e60f5e36
Bilal Bejja <72463941+bilalbejja-collab@users.noreply.github.com>
2021-05-25 17:42:03 +0200

    finalizada la parte de proyectos

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -6,8 +6,8 @@ export default class Project extends Component {
   };
 
   handleInfo = () => {
-    
-  }
+    showInfo: !this.state.showInfo;
+  };
 
   render() {
     let { name, languagesIcons, source, info, picture } = this.props.item;
@@ -20,10 +20,34 @@ export default class Project extends Component {
           ))}
         </div>
         <h3>{name}</h3>
-        <img src={picture} alt="" />
-        <span className="infos">
+        <img src={picture} alt="" onClick={this.handleInfo} />
+        <span className="infos" onClick={this.handleInfo}>
           <i className="fas fa-plus-circle"></i>
         </span>
+
+        {this.state.showInfo && (
+          <div className="showInfo">
+            <div className="infoContent">
+              <div className="head">
+                <h2>{name}</h2>
+                <div className="sourceCode">
+                  <a
+                    href={source}
+                    rel="noopener noreferrer"
+                    className="button"
+                    target="_blank"
+                  >
+                    Code source
+                  </a>
+                </div>
+                <p className="text">{info}</p>
+              </div>
+              <div className="button return" onClick={this.handleInfo}>
+                Retourner sur la page
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
